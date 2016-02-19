775db9310948de6ebcd81143572372d9a2999c20
Vitaliy Yarkun <AdamFox@macbook-pro-vitaliy.local>
2016-02-19 15:16:11 +0200

    UI basic config

---
+++ ImgurMobile/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  ImgurMobile
+//
+//  Created by Vitaliy Yarkun on 19.02.16.
+//  Copyright © 2016 Melany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UIViewController
+
+@end
---
+++ ImgurMobile/SocialViewController.m
@@ -0,0 +1,37 @@
+//
+//  SocialViewController.m
+//  ImgurMobile
+//
+//  Created by Vitaliy Yarkun on 19.02.16.
+//  Copyright © 2016 Melany. All rights reserved.
+//
+
+#import "SocialViewController.h"
+
+@interface SocialViewController ()
+
+@end
+
+@implementation SocialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
