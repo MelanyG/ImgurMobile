57f70380cb8ab7e747cbd1cfb21db6ae8c27d95a
alex4eetah <alex4eetah@gmail.com>
2016-02-20 18:37:13 +0200

    www

--- ImgurMobile/EditViewController.m
+++ ImgurMobile/EditViewController.m
@@ -11,6 +11,12 @@
 @interface EditViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) UIScrollView *scrollView;
+
+@property (strong, nonatomic) CIContext *ctx;
+@property (strong, nonatomic) CIImage *beginImage;
+@property (strong, nonatomic) CIFilter *filter;
+
 @end
 
 @implementation EditViewController
@@ -18,28 +24,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.image = [UIImage imageNamed:@"sea"];
     
-    CIContext *ctx = [CIContext contextWithOptions:nil];
     
-    UIImage *image = [UIImage imageNamed:@"sea"];
+    self.ctx = [CIContext contextWithOptions:nil];
+
+    self.beginImage = [CIImage imageWithCGImage:self.image.CGImage];
     
-    CIImage *beginImage = [CIImage imageWithCGImage:image.CGImage];
-    
-    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"
+    self.filter = [CIFilter filterWithName:@"CISepiaTone"
                                   keysAndValues:
-                        kCIInputImageKey, beginImage,
+                        kCIInputImageKey, self.beginImage,
                         @"inputIntensity", @0.8, nil];
     
-    CIImage *outImage = [filter outputImage];
+    CIImage *outImage = [self.filter outputImage];
     
-    CGImageRef cgImg = [ctx createCGImage:outImage fromRect:[outImage extent]];
+    CGImageRef cgImg = [self.ctx createCGImage:outImage fromRect:[outImage extent]];
     
     self.imageView.image = [UIImage imageWithCGImage:cgImg];
     
     CGImageRelease(cgImg);
 }
 
+- (IBAction)intensityBeenChanged:(UISlider *)sender
+{
+    float slideValue = sender.value;
+    
+    [self.filter setValue:@(slideValue)
+              forKey:@"inputIntensity"];
+    CIImage *outImage = [self.filter outputImage];
+    
+    CGImageRef cgImg = [self.ctx createCGImage:outImage fromRect:[outImage extent]];
+    
+    self.imageView.image = [UIImage imageWithCGImage:cgImg];
+    
+    CGImageRelease(cgImg);
+}
 
+- (void)addScrollView
+{
+    
+}
 
 
 @end
