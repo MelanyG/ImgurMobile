fedeaafc0d0fcc87c80b22700e89e6990ee594a4
Vitaliy Yarkun <AdamFox@mbp-vitaliy.dlink>
2016-02-28 16:37:54 +0200

    get request

--- ImgurMobile/SocialViewController.m
+++ ImgurMobile/SocialViewController.m
@@ -7,31 +7,65 @@
 //
 
 #import "SocialViewController.h"
+#import "RESTAPI.h"
+@interface SocialViewController () <RESTAPIDelegate>
 
-@interface SocialViewController ()
+@property (strong, nonatomic) RESTAPI *restApi;
+@property (strong, nonatomic) NSCharacterSet* set;
 
 @end
 
 @implementation SocialViewController
 
-- (void)viewDidLoad {
+
+-(RESTAPI *)restApi
+{
+    if (!_restApi)
+    {
+        _restApi = [[RESTAPI alloc] init];
+    }
+    return _restApi;
+}
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.set = [NSCharacterSet URLQueryAllowedCharacterSet];
+    [self httpGetRequest];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)httpGetRequest
+{
+    NSString *str = @"https://api.imgur.com/3/image.json";
+    str = [str stringByAddingPercentEncodingWithAllowedCharacters:self.set];
+    NSURL *url = [NSURL URLWithString:str];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:GET];
+    self.restApi.delegate = self;
+    [self.restApi httpRequest:request];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)httpPostRequest
+{
+    NSString *postBody = @"";
+    NSString *str = @"";
+    str = [str stringByAddingPercentEncodingWithAllowedCharacters:self.set];
+    NSURL *url = [NSURL URLWithString:str];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:POST];
+    [request setHTTPBody:[postBody dataUsingEncoding:NSUTF8StringEncoding]];
+    self.restApi.delegate = self;
+    [self.restApi httpRequest:request];
 }
-*/
+
+- (void)getReceivedData:(NSMutableData *)data sender:(RESTAPI *)sender
+{
+    NSError *error = nil;
+    NSDictionary *receivedData =[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+    
+}
+
+
+
 
 @end
