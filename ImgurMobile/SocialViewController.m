7fd5a8107d3782a3860c373ea8d64fe7cdd6cc7d
Vitaliy Yarkun <AdamFox@mbp-vitaliy.dlink>
2016-03-01 19:26:18 +0200

    image update

--- ImgurMobile/SocialViewController.m
+++ ImgurMobile/SocialViewController.m
@@ -39,6 +39,7 @@
     [super viewDidLoad];
     self.set = [NSCharacterSet URLQueryAllowedCharacterSet];
     self.accessToken = [ImgurAccessToken sharedToken].token;
+    self.socialImage.image = self.image;
     if ([self.post.postDescription isKindOfClass:[NSString class]]) {
         self.socialImageDescription.text = self.post.postDescription;
     }
