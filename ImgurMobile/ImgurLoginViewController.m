0d84275df4afa1558db7b7e124b23e954a8ccd52
Melany <gmr@inbox.ru>
2016-03-03 11:47:53 +0200

    login

--- ImgurMobile/ImgurLoginViewController.m
+++ ImgurMobile/ImgurLoginViewController.m
@@ -11,11 +11,16 @@
 #import "imgurServerManager.h"
 #import "ImgurUser.h"
 
+static BOOL firstTimeAppear;
+
 @interface ImgurLoginViewController ()<UIWebViewDelegate>
 
 @property (copy, nonatomic) ASLoginCompletionBlock completionBlock;
-@property (weak, nonatomic) UIWebView* webView;
-@property (assign, nonatomic) BOOL firstTimeAppear;
+@property (strong, nonatomic) UIWebView* webView;
+//@property (assign, nonatomic) BOOL firstTimeAppear;
+@property (strong, nonatomic) NSURLRequest* request;
+
+
 
 @end
 
@@ -34,21 +39,21 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.firstTimeAppear = YES;
+    //firstTimeAppear = YES;
     CGRect r = self.view.bounds;
     r.origin = CGPointZero;
     self.token = [ImgurAccessToken sharedToken];
     
     
     
-    UIWebView* webView = [[UIWebView alloc] initWithFrame:r];
+    self.webView = [[UIWebView alloc] initWithFrame:r];
     
-    webView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    self.webView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     
-    [self.view addSubview:webView];
-    webView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y+44, self.view.frame.size.width, self.view.frame.size.height-44);
+    [self.view addSubview:self.webView];
+    self.webView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y+44, self.view.frame.size.width, self.view.frame.size.height-44);
     
-    self.webView = webView;
+    //self.webView = webView;
       // UINavigationBar *navbar = [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 60)];
     //do something like background color, title, etc you self
     //[self.view addSubview:navbar];
@@ -60,37 +65,36 @@
 
     
     //self.navigationItem.title = @"Login";
-    self.navigationController.navigationBar.tintColor = [UIColor redColor];
-    NSString* urlString =
+        NSString* urlString =
     @"https://api.imgur.com/oauth2/authorize?"
     "client_id=b765b2f66708b7a&"
     "response_type=token";
     
     NSURL* url = [NSURL URLWithString:urlString];
     
-    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+  self.request = [NSURLRequest requestWithURL:url];
     
-    webView.delegate = self;
     
-    [webView loadRequest:request];
+    self.webView.delegate = self;
+    
     
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     
     [super viewDidAppear:animated];
+    [self.webView loadRequest:self.request];
+//    if (!firstTimeAppear) {
+//        firstTimeAppear = YES;
     
-    if (self.firstTimeAppear) {
-        self.firstTimeAppear = NO;
         
+//        [[imgurServerManager sharedManager] authorizeUser:^(imgurUser *user) {
+//         
+//            NSLog(@"AUTHORIZED!");
+//            NSLog(@"%@", user.account_id);
+//        }];
         
-        [[imgurServerManager sharedManager] authorizeUser:^(imgurUser *user) {
-            
-            NSLog(@"AUTHORIZED!");
-            NSLog(@"%@", user.account_id);
-        }];
-        
-    }
+ //   }
     
 }
 
@@ -109,94 +113,85 @@
 
 #pragma mark - UIWebViewDelegete
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+- (BOOL)webView:(UIWebView *)webView
+shouldStartLoadWithRequest:(NSURLRequest *)request
+ navigationType:(UIWebViewNavigationType)navigationType {
     //ImgurAccessToken* token = [[ImgurAccessToken alloc] init];
     
     //#access_token=83cbcfabbe307897a21d062df1515fcad3d66765&expires_in=2419200&token_type=bearer&refresh_token=621ec2e692b00b4d8336aa21c06ae1ceca308a31&account_username=MostPerfectUser&account_id=31463385
-    
-    
+      if(!firstTimeAppear)
+    {
     if ([[[request URL] description] rangeOfString:@"#access_token="].location != NSNotFound)
     {
-        
-        NSLog(@"%@", [request URL]);
-        
-        NSString* query = [[request URL] description];
-        
-        NSArray* array = [query componentsSeparatedByString:@"#"];
-        
-        if ([array count] > 1)
-        {
-            query = [array lastObject];
-        }
-        
-        
-        NSArray* pairs = [query componentsSeparatedByString:@"&"];
-        
-        for (NSString* pair in pairs)
-        {
+      
+        firstTimeAppear = YES;
             
-            NSArray* values = [pair componentsSeparatedByString:@"="];
-            
-            if ([values count] == 2)
+            NSLog(@"%@", [request URL]);
+            NSString* query = [[request URL] description];
+            NSArray* array = [query componentsSeparatedByString:@"#"];
+            if ([array count] > 1)
             {
-                
-                NSString* key = [values firstObject];
-                
-                if ([key isEqualToString:@"access_token"])
-                {
-                    self.token.token = [values lastObject];
-                }
-                else if ([key isEqualToString:@"refresh_token"])
-                {
-                    self.token.refresh_token = [values lastObject];
-                }
-                else if ([key isEqualToString:@"expires_in"])
-                {
-                    
-                    NSTimeInterval interval = [[values lastObject] doubleValue];
-                    
-                    self.token.expirationDate = [NSDate dateWithTimeIntervalSinceNow:interval];
-                    NSLog(@"Exp: %@", self.token.expirationDate);
-                }
-                else if ([key isEqualToString:@"account_username"]) {
-                    
-                    self.token.userName = [values lastObject];
-                }
-                else if ([key isEqualToString:@"account_id"]) {
-                    
-                    self.token.accountID = [values lastObject];
-                }
-                
+                query = [array lastObject];
             }
+            
+            NSArray* pairs = [query componentsSeparatedByString:@"&"];
+            for (NSString* pair in pairs)
+            {
+                NSArray* values = [pair componentsSeparatedByString:@"="];
+                if ([values count] == 2)
+                {
+                    NSString* key = [values firstObject];
+                    if ([key isEqualToString:@"access_token"])
+                    {
+                        self.token.token = [values lastObject];
+                    }
+                    else if ([key isEqualToString:@"refresh_token"])
+                    {
+                        self.token.refresh_token = [values lastObject];
+                    }
+                    else if ([key isEqualToString:@"expires_in"])
+                    {
+                        NSTimeInterval interval = [[values lastObject] doubleValue];
+                        self.token.expirationDate = [NSDate dateWithTimeIntervalSinceNow:interval];
+                        NSLog(@"Exp: %@", self.token.expirationDate);
+                    }
+                    else if ([key isEqualToString:@"account_username"]) {
+                        
+                        self.token.userName = [values lastObject];
+                    }
+                    else if ([key isEqualToString:@"account_id"]) {
+                        
+                        self.token.accountID = [values lastObject];
+                    }
+                }
+            }
+            
+            self.token.dayOfLogin  =  [NSDate date];
+            NSLog(@"day of login: %@", self.token.dayOfLogin);
+            self.webView.delegate = nil;
+            if (self.completionBlock)
+            {
+                self.completionBlock(self.token);
+            }
+            // Store the data
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            
+            [defaults setObject:self.token.userName forKey:@"userName"];
+            [defaults setObject:self.token.token forKey:@"access_token"];
+            [defaults setObject:self.token.refresh_token forKey:@"refresh_token"];
+            [defaults setObject:self.token.accountID forKey:@"account_id"];
+            [defaults setObject:self.token.expirationDate forKey:@"expires_in"];
+            [defaults setObject:self.token.dayOfLogin forKey:@"dayOfLogin"];
+            
+            NSLog(@"Updated User is:%@", self.token.userName);
+            
+            [self dismissViewControllerAnimated:YES
+                                     completion:nil];
+            return NO;
         }
-        
-      self.token.dayOfLogin  =  [NSDate date];
-    
-     NSLog(@"day of login: %@", self.token.dayOfLogin);
-        
-        
-        self.webView.delegate = nil;
-        
-        if (self.completionBlock) {
-            self.completionBlock(self.token);
-        }
-        
-        // Store the data
-        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-        
-        [defaults setObject:self.token.userName forKey:@"userName"];
-        [defaults setObject:self.token.token forKey:@"access_token"];
-        [defaults setObject:self.token.refresh_token forKey:@"refresh_token"];
-        [defaults setObject:self.token.accountID forKey:@"account_id"];
-        [defaults setObject:self.token.expirationDate forKey:@"expires_in"];
-        [defaults setObject:self.token.dayOfLogin forKey:@"dayOfLogin"];
-        
-        [self dismissViewControllerAnimated:YES
-                                 completion:nil];
-        
-        return NO;
     }
-    
+//    [self dismissViewControllerAnimated:YES
+//                             completion:nil];
     return YES;
 }
 
