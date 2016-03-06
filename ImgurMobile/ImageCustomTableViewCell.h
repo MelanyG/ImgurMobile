ee3c23f21c689b6f81770cb5800424cd65552c2c
Vitaliy Yarkun <AdamFox@mbp-vitaliy.dlink>
2016-03-06 22:51:13 +0200

    Preparing for Image Cell

---
+++ ImgurMobile/ImageCustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ImageCustomTableViewCell.h
+//  ImgurMobile
+//
+//  Created by Vitaliy Yarkun on 06.03.16.
+//  Copyright © 2016 Melany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCustomTableViewCell : UITableViewCell
+
+@end
---
+++ ImgurMobile/ImageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageTableViewController.h
+//  ImgurMobile
+//
+//  Created by Vitaliy Yarkun on 06.03.16.
+//  Copyright © 2016 Melany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTableViewController : UITableViewController
+
+@end
